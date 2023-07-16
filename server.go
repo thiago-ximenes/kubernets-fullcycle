8a24c1346d6d8b626530235449df02957ede9a85
thiagoximenes <thiagoximeneslima@gmail.com>
2023-07-16 19:01:49 -0300

    feat(configMap): inject family member to go-server configMap

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/configMap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
@@ -17,4 +20,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s years old.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("/go/my-family/family.txt")
+	if err != nil {
+		log.Fatalf("failed to read file: %s", err)
+	}
+
+	fmt.Fprintf(w, "My Family: %s", string(data))
 }
