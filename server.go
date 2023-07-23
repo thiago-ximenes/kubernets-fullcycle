b0e3ab144111a387ac318640f674634fa5a44020
thiagoximenes <thiagoximeneslima@gmail.com>
2023-07-23 18:03:18 -0300

    feat(livenessProbe): app health checks

--- server.go
+++ server.go
@@ -6,13 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configMap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
+
+var startedAt = time.Now()
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 
@@ -30,4 +35,23 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "My Family: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s/n Password: %s", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Durantion: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
